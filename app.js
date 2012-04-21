68e1f19dffb10ab839e195414451fd8fe7f74e76
David Robbins <robbins.david@gmail.com>
2012-04-21 16:14:56 -0500

    Reformatted codebase

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
-var requirejs=require('requirejs');
-var couchapp = require('couchapp');
-var path = require('path');
-var fs = require('fs');
+;(function(){
 
-var ignore = JSON.parse(fs.readFileSync(path.join(__dirname, ".couchappignore")).toString()),
-    loadFileOptions = {ignore: ignore},
-    validate_doc_update = function(newDoc, savedDoc, userCtx){
-        if(!userCtx.name){
-          throw({forbidden : "You must be logged in to enter patient records"});
-        }
-        if(newDoc.type === "data_managment_form"){
-          if(newDoc.opername !== userCtx.name){
-            throw({forbidden : "opername required to match your username"});
-          }
-        }
-    };
-var ddoc = {
-    _id: '_design/Dashcouch',
-    views : couchapp.loadFiles('./views', loadFileOptions),
-    evently : couchapp.loadFiles('./evently', loadFileOptions),
-    lists : couchapp.loadFiles('./lists', loadFileOptions),
-    shows : {},
-    vendor : couchapp.loadFiles('./vendor', loadFileOptions),
-    validate_doc_update : validate_doc_update
-};
+  var couchapp = require('couchapp');
+  var path = require('path');
+  var fs = require('fs');
 
-module.exports = ddoc;
-function loadAllAttachments(ddoc, dir){
-    var listings = fs.readdirSync(dir);
+  var ignore
+      ,loadFileOptions
+      ,validateDocUpdate
+      ,ddoc
+      ,loadAllAttachments
+
+  validateDocUpdate = function validateDocUpdate (newDoc, savedDoc, userCtx) {
+    if(!userCtx.name){
+      throw({forbidden : "You must be logged in to enter patient records"})
+    }
+    if(newDoc.type === "data_managment_form"){
+      if(newDoc.opername !== userCtx.name){
+        throw({forbidden : "opername required to match your username"})
+      }
+    }
+  }
+
+  loadAllAttachments = function loadAllAttachments (ddoc, dir) {
+    var listings = fs.readdirSync(dir)
     listings.forEach(function(listing){
-        var file = path.join(dir, listing),
-            stat = fs.statSync(file),
-            prefix = "";
-        if(stat.isFile()){return;}
+        var file = path.join(dir, listing)
+            ,stat = fs.statSync(file)
+            ,prefix = ""
+        if(stat.isFile()){return }
         if(path.basename(file) === "_attachments"){
-            prefix = path.dirname(file).slice(__dirname.length+1);
-            couchapp.loadAttachments(ddoc, file, prefix);
+            prefix = path.dirname(file).slice(__dirname.length+1)
+            couchapp.loadAttachments(ddoc, file, prefix)
         } else {
-            loadAllAttachments(ddoc, file);
+            loadAllAttachments(ddoc, file)
         }
     });
-}
+  }
 
-loadAllAttachments(ddoc, __dirname);
+  ignore = JSON.parse(fs.readFileSync(path.join(__dirname, ".couchappignore")).toString())
+  loadFileOptions = {ignore: ignore}
+
+  // To load a subdirectory: views : couchapp.loadFiles('./views', loadFileOptions)
+  ddoc = { _id : '_design/DashCouch'
+    ,views : null
+    ,evently : null
+    ,lists : null
+    ,shows : null
+    ,vendor : null
+    ,validate_doc_update : validateDocUpdate
+  }
+
+  loadAllAttachments(ddoc, __dirname);
+  module.exports = ddoc
+}());
